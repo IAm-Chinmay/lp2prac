38d381c540116ab2260e034332991545d469f8fd
Chinmay Mulay <84238212+IAm-Chinmay@users.noreply.github.com>
2024-05-10 10:52:09 +0530

    Update T190524281.py

--- T190524281.py
+++ T190524281.py
@@ -1,1 +1,14 @@
 print("Hello GoOoGle App Engine")
+def add(n):
+  add = 0
+  for i in n:
+    add += i
+  return add
+
+a = int(input("How many elements to be added :"))
+ele = []
+for i in range(a):
+  k = int(input("Enter input for element no. { }",i))
+  ele.append(k)
+
+add(ele)
