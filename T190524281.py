1874f8835744879227c3dc26032a6d900fbacff7
Chinmay Mulay <84238212+IAm-Chinmay@users.noreply.github.com>
2024-05-10 11:04:57 +0530

    Update T190524281.py

--- T190524281.py
+++ T190524281.py
@@ -5,10 +5,10 @@ def add(n):
     add += i
   return add
 
-a = int(input("How many elements to be added :"))
+a = int(input(f"How many elements to be added :"))
 ele = []
 for i in range(a):
-  k = int(input("Enter input for element no. {i}"))
+  k = int(input(f"Enter input for element no. {i}"))
   ele.append(k)
 
 a = add(ele)
